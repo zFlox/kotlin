2be0eb18aa58475492a88d9a60e1146b855a64e0
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2019-12-23 17:45:40 +0300

    [Gradle, JS] Copy JS plugin for IR

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/dsl/KotlinProjectExtension.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/dsl/KotlinProjectExtension.kt
@@ -25,10 +25,9 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetContainer
 import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJsCompilation
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinOnlyTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinWithJavaTarget
 import org.jetbrains.kotlin.gradle.targets.js.KotlinJsTarget
+import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrTarget
 import kotlin.reflect.KClass
 
 private const val KOTLIN_PROJECT_EXTENSION_NAME = "kotlin"
@@ -70,7 +69,7 @@ abstract class KotlinSingleTargetExtension : KotlinProjectExtension() {
 }
 
 abstract class KotlinSingleJavaTargetExtension : KotlinSingleTargetExtension() {
-    override abstract val target: KotlinWithJavaTarget<*>
+    abstract override val target: KotlinWithJavaTarget<*>
 }
 
 open class KotlinJvmProjectExtension : KotlinSingleJavaTargetExtension() {
@@ -91,6 +90,19 @@ open class KotlinJsProjectExtension : KotlinSingleTargetExtension() {
     override lateinit var target: KotlinJsTarget
 
     open fun target(body: KotlinJsTarget.() -> Unit) = target.run(body)
+
+    @Deprecated(
+        "Needed for IDE import using the MPP import mechanism",
+        level = DeprecationLevel.HIDDEN
+    )
+    fun getTargets() =
+        target.project.container(KotlinTarget::class.java).apply { add(target) }
+}
+
+open class KotlinJsIrProjectExtension : KotlinSingleTargetExtension() {
+    override lateinit var target: KotlinJsIrTarget
+
+    open fun target(body: KotlinJsIrTarget.() -> Unit) = target.run(body)
 
     @Deprecated(
         "Needed for IDE import using the MPP import mechanism",
