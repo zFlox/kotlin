aa3d213c7868956aed371240c7986299b02649f1
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2019-12-27 16:04:36 +0100

    JVM_IR: Also check attributes for suspendImpls when generating continuation
    Otherwise, it would not generate continuations for overloads.

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/AddContinuationLowering.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/AddContinuationLowering.kt
@@ -522,6 +522,7 @@ private class AddContinuationLowering(private val context: JvmBackendContext) : 
                 }
             })
         }
+        static.copyAttributes(irFunction)
         return static
     }
 
