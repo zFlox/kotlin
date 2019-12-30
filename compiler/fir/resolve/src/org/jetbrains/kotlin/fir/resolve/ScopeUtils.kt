338d997c7c0a3fe33d10b178f6546eb7b24dfe44
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-12-30 19:42:52 +0300

    Exception fix (just a stub)

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/ScopeUtils.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/ScopeUtils.kt
@@ -59,7 +59,7 @@ fun ConeKotlinType.scope(useSiteSession: FirSession, scopeSession: ScopeSession)
                 FirIntegerLiteralTypeScope(useSiteSession)
             } as FirScope
         }
-        else -> error("Failed type $this")
+        else -> null //error("Failed type $this")
     }
 }
 
