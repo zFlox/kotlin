9828b3f7e48cc3b2fc8395abb5004ba2e75eaded
Mark Punzalan <markpun@google.com>
2020-01-08 02:08:50 -0800

    [JVM IR] Use names of local functions in names of local classes.
    This undoes changes in
    https://github.com/JetBrains/kotlin/commit/fbe66c3496d082d2d487b2c39673f0cd4ac5b70a
    which broke calculation of the simple name of local classes in
    reflection (the enclosing method was not a substring of the name of the
    local class).

--- compiler/testData/checkLocalVariablesTable/itInReturnedLambda.kt
+++ compiler/testData/checkLocalVariablesTable/itInReturnedLambda.kt
@@ -6,6 +6,12 @@ fun foo() {
     }
 }
 
+// JVM_TEMPLATES
 // METHOD : ItInReturnedLambdaKt$foo$1$1.invoke(I)V
 // VARIABLE : NAME=this TYPE=LItInReturnedLambdaKt$foo$1$1; INDEX=0
+// VARIABLE : NAME=it TYPE=I INDEX=1
+
+// JVM_IR_TEMPLATES
+// METHOD : ItInReturnedLambdaKt$foo$bar$1.invoke(I)V
+// VARIABLE : NAME=this TYPE=LItInReturnedLambdaKt$foo$bar$1; INDEX=0
 // VARIABLE : NAME=it TYPE=I INDEX=1

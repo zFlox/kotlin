zFlox/kotlin

commit fbe66c3496d082d2d487b2c39673f0cd4ac5b70a

JVM IR: fix another naming inconsistency with old backend
This fixes slice rewrite stacktraces from RecordNamesForKotlinTypeMapper
which are printed out when running some JVM IR tests (even though
they're ignored), e.g. localClassSimpleName.kt

compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/InventNamesForLocalClasses.kt
@@ -107,6 +107,12 @@ class InventNamesForLocalClasses(private val context: JvmBackendContext) : FileL
                         localFunctionNames[(declaration as IrFunction).symbol] = name
                     }
                 }
+                declaration is IrFunction && declaration.parent !is IrClass -> {
+                    // In the old backend, only names of non-local functions are stored in names of anonymous classes. All other names
+                    // are replaced with indices. For example, a local class `L` in a top-level function `f` will have the name `...$f$L`,
+                    // but inside a local function `g` (which is in `f`) it will have the name `...$f$1$L` (_not_ `...$f$g$L`).
+                    inventName(null, data)
+                }
                 enclosingName != null -> "$enclosingName$$simpleName"
                 else -> simpleName
             }

compiler/testData/checkLocalVariablesTable/itInReturnedLambda.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND: JVM_IR
 fun foo() {
     fun bar() : (Int) -> Unit {
         return {

commit 9828b3f7e48cc3b2fc8395abb5004ba2e75eaded

[JVM IR] Use names of local functions in names of local classes.
This undoes changes in
https://github.com/JetBrains/kotlin/commit/fbe66c3496d082d2d487b2c39673f0cd4ac5b70a
which broke calculation of the simple name of local classes in
reflection (the enclosing method was not a substring of the name of the
local class).

compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/InventNamesForLocalClasses.kt
@@ -108,12 +108,6 @@ class InventNamesForLocalClasses(private val context: JvmBackendContext) : FileL
                         localFunctionNames[(declaration as IrFunction).symbol] = name
                     }
                 }
-                declaration is IrFunction && declaration.parent !is IrClass -> {
-                    // In the old backend, only names of non-local functions are stored in names of anonymous classes. All other names
-                    // are replaced with indices. For example, a local class `L` in a top-level function `f` will have the name `...$f$L`,
-                    // but inside a local function `g` (which is in `f`) it will have the name `...$f$1$L` (_not_ `...$f$g$L`).
-                    inventName(null, data)
-                }
                 enclosingName != null -> "$enclosingName$$simpleName"
                 else -> simpleName
             }

compiler/testData/checkLocalVariablesTable/itInReturnedLambda.kt
@@ -6,6 +6,12 @@ fun foo() {
     }
 }
 
+// JVM_TEMPLATES
 // METHOD : ItInReturnedLambdaKt$foo$1$1.invoke(I)V
 // VARIABLE : NAME=this TYPE=LItInReturnedLambdaKt$foo$1$1; INDEX=0
+// VARIABLE : NAME=it TYPE=I INDEX=1
+
+// JVM_IR_TEMPLATES
+// METHOD : ItInReturnedLambdaKt$foo$bar$1.invoke(I)V
+// VARIABLE : NAME=this TYPE=LItInReturnedLambdaKt$foo$bar$1; INDEX=0
 // VARIABLE : NAME=it TYPE=I INDEX=1

compiler/testData/codegen/box/reflection/classes/localClassSimpleName.kt
@@ -1,5 +1,4 @@
 // IGNORE_BACKEND_FIR: JVM_IR
-// IGNORE_BACKEND: JVM_IR
 // IGNORE_BACKEND: NATIVE
 // WITH_RUNTIME
 